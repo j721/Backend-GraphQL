6afe425e2dfdb35c46a95d7f2bbae606a7b224f1
j721 <juliadvu@gmail.com>
2020-10-09 12:18:15 -0500

    setting up basic server

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql').graphqlHTTP
+
 const app = express()
 
+//routes
+app.use('/graphql', expressGraphQL({
+    graphiql: true
+}));
+
+
+
+
+//Port
 app.listen(5000, ()=>{
     console.log('server is running')
 })
