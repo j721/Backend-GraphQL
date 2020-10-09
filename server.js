74c241c23a1602b9b5f72f288e922fe5c912602c
j721 <juliadvu@gmail.com>
2020-10-09 12:36:33 -0500

    created basic schema

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql').graphqlHTTP
+const{
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql')
 
 const app = express()
 
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'Hello World',
+        fields: () =>({
+            message: {
+            type: GraphQLString,
+            resolve: ()=> 'Hello World'}
+        })
+    })
+})
+
 //routes
 app.use('/graphql', expressGraphQL({
-    graphiql: true
+    schema: schema,
+    graphiql: true      //allows you to run graphiQL in local server
 }));
 
 
