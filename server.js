2651aa336cfb91ce897110e517432292e429db75
j721 <juliadvu@gmail.com>
2020-10-11 02:08:28 -0500

    created another subfield called book to grab a single book in the RootQuery

--- server.js
+++ server.js
@@ -63,6 +63,14 @@ const RootQueryType = new GraphQLObjectType({
   name: "Query",
   description: "Root Query",
   fields: () => ({
+    book: {
+        type: BookType,
+        description: "A Single Book",
+        args:{
+            id: {type: GraphQLInt}
+        },
+        resolve: (parent, args) => books.find(book=> book.id == args.id)
+      },
     books: {
       type: new GraphQLList(BookType),
       description: "List of Books",
